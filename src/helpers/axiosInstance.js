2bf198c30ededa1d8302dbc69e8b563750cfa948
Bhargav Mule <bhargavmule16@gmail.com>
2024-02-13 18:20:04 +0530

    fixes

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8090/api/v1";
+const BASE_URL = "https://onlyfriends-backend-production-a2b2.up.railway.app/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
