0b72403a6fbb4ba422d9d222a2e4654d694f1c44
Bhargav Mule <bhargavmule16@gmail.com>
2024-02-13 18:14:10 +0530

    fixes

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,13 +1,29 @@
-import axios from "axios"
+// helpers/axiosInstance.js
 
-const BASE_URL = "https://onlyfriends-backend-production-a2b2.up.railway.app/api/v1"
+import axios from 'axios';
 
+const BASE_URL = "http://localhost:8090/api/v1";
 
 const axiosInstance = axios.create({
-    baseURL:BASE_URL,
-    withCredentials:true
-})
+  baseURL: BASE_URL,
+  withCredentials: true
+});
 
+axiosInstance.interceptors.request.use(
+  config => {
+    
+    const authToken = localStorage.getItem('cookie');
 
+    
+    if (authToken) {
+      config.headers['Authorization'] = `Bearer ${authToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
