8341f967c9f9d5b0d23bbadd41ae8190da70e622
Bhargav Mule <bhargavmule16@gmail.com>
2024-02-13 08:10:58 +0530

    fixes

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 const initialState = {
     authData:{
-        data: JSON.parse(localStorage.getItem('data')) || {},
+        data: localStorage.getItem('data') || {},
         isLoggedIn : localStorage.getItem('isLoggedIn') || false,
     },
     data:{}
