aeb8d4c1577281f526cbeeaf2dad892aaa0fc02e
Bhargav Mule <bhargavmule16@gmail.com>
2024-02-20 22:17:20 +0530

    updates

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import bgDesktop from '../assets/Frame 5.png';
-import bgMobile from '../assets/ab.png';
+import bgMobile from '../assets/abc.png';
 
 
 function LandingPage() {
